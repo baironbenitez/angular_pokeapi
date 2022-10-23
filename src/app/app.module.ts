763efdf738595a4d55058586ab7f7cb03bd2d37e
bairon.benitez <ingbaironsis@gmail.com>
2022-10-23 08:09:50 -0500

    Se agrega modulo de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LayoutsModule } from './layouts/layouts.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
+    AppRoutingModule,
     LayoutsModule
   ],
   providers: [],
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'pokemons',
+    loadChildren:()=>import('./pokemons/pokemons.module').then(m=>m.PokemonsModule)
+  },
+  {
+    path:'',
+    redirectTo:'pokemons',
+    pathMatch:'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
