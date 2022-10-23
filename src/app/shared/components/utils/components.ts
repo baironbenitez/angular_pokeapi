e18a2693ddc8b8c441b0e5193922ce3b64d209b8
bairon.benitez <ingbaironsis@gmail.com>
2022-10-23 08:47:47 -0500

    Se agrega componente para tipo de pokemon

--- src/app/shared/components/utils/components.ts
+++ src/app/shared/components/utils/components.ts
@@ -1,5 +1,11 @@
 import { CardPokemonComponent } from "@Components/card-pokemon/card-pokemon.component";
+import { LoadingComponent } from "@Components/loading/loading.component";
+import { NotDataComponent } from "@Components/not-data/not-data.component";
+import { TypeComponent } from "@Components/type/type.component";
 
 export default [
-    CardPokemonComponent
+    CardPokemonComponent,
+    LoadingComponent,
+    TypeComponent,
+    NotDataComponent
 ]
